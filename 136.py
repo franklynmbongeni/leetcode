72c5aaf437ff8450d56dcd11e2becc95e9ae95e2
franklyn <ngwenyambongeni04@gmai.com>
2026-02-18 13:56:24 +0100

    updated the code explanation for the 136 solution

--- 136.py
+++ 136.py
@@ -1,5 +1,5 @@
 #the solution uses bit manipulation , elements of the same value will cancel each other out
-#and therefore the single number will be left
+#and therefore the single number will be left this solution satisfy the constant space constraint 
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
