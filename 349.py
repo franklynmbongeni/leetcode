3f58421b139e9a231c73dd752b9ae66ddf56772e
franklyn <ngwenyambongeni04@gmai.com>
2026-02-19 17:04:27 +0100

    solution to 349.py

--- 349.py
+++ 349.py
@@ -1,1 +1,12 @@
 #Intersection of Two Arrays II
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        c = Counter(nums1) #this is a frequency counter it stores the result in a dictionery
+        result = [] #this will be the output
+
+        for num in nums2:
+            if c[num] > 0: #this check the frequency if num is also available in the dictionery and the count is greater than zero then thats an intersaction
+                result.append(num) #if there is an intersaction we add the result to RESULT
+                c[num] -= 1 #we have to reduce the frequency since we added it to result
+        return result
