213629d24719c89401f6f476c94eceeb4566cd2f
franklynmbongeni <ngwenyambongenifranklyn@gmail.com>
2026-03-12 21:22:46 +0100

    second solution to reverse string

--- reverse_string.py
+++ reverse_string.py
@@ -12,3 +12,18 @@ class Solution:
             s[i], s[j] = s[j], s[i]
             i += 1
             j -= 1
+
+        #this solution runs at at o(n) time and uses o(1) space
+
+
+#the second solution
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+
+        """
+
+        s[:] = s[::-1] #this solution looks great as it is short however it uses o(n) space because of slicing
