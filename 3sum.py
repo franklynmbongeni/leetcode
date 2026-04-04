6659f35ef6638fae036c515fe0dedb0e09dcd7f0
franklynmbongeni <ngwenyambongenifranklyn@gmail.com>
2026-04-04 23:57:22 +0200

    partial solve of the 3 sum problem

--- 3sum.py
+++ 3sum.py
@@ -1,1 +1,24 @@
 
+
+
+def threeSum(nums: list[int]) -> list[list[int]]:
+    nums.sort()
+    results = []
+
+    for i in range(len(nums)):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+
+        left = i + 1
+        right = len(nums) - 1
+
+        while left < right:
+            total = nums[i] + nums[left] + nums[right]
+
+            if total == 0 :
+                results.append
+
+
+numer = [-1,0,1,2,-1,-4]
+
+print(threeSum(numer))
