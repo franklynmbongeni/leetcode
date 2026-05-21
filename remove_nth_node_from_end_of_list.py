d8a8bf4c7c1f252d1c976dc10bb6533708821f1e
franklynmbongeni <ngwenyambongenifranklyn@gmail.com>
2026-05-21 22:33:36 +0200

    delete node in a linked list

--- remove_nth_node_from_end_of_list.py
+++ remove_nth_node_from_end_of_list.py
@@ -12,14 +12,18 @@ class Solution:
   left = dummy
   right = head
 
-  while n > 0 and right:
-   right = right.next
-   n -= 1
+  while n > 0 and right: # this is to iterate right to be n+1 positions ahead of left(we use loop since right = head + n
+     right = right.next  # wont work
+     n -= 1
+     # this ensures that when right runs out we know that left is on the nTh - 1 position from the
+                   # end of the list so we can simply assign the .next to = .next.next
+
 
   while left and right:
    left = left.next
    right = right.next
 
-  left.next = left.next.next
-
+  left.next = left.next.next #this is to break the connection
+  # 1 -> 2 -> 3 -> 4 -> 5  output = 1 -> 2 -> 3 -> 5
+  
   return dummy.next
