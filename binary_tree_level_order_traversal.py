58b3439d357460e332491701598ff42c10ecc82d
franklynmbongeni <ngwenyambongenifranklyn@gmail.com>
2026-07-29 17:24:47 +0200

    convert_sorted_array_to_binary_search_tree problem

--- binary_tree_level_order_traversal.py
+++ binary_tree_level_order_traversal.py
@@ -1,31 +1,22 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+from collections import deque
+def func():
+    if root is None:
+        return None
 
-        if root is None:
-            return None
+    queue = deque()
+    queue.append(root)
+    ans = []
 
-        queue = deque()
-        queue.append(root)
-        ans = []
+    while queue:
+        level = []
+        n = len(queue)
+        for i in range(n):
+            node = queue.popleft()
+            level.append(node.val)
 
-        while queue:
-            level = []
-            n = len(queue)
-            for i in range(n):
-                node = queue.popleft()
-                level.append(node.val)
-
-                if node.left:
-                    queue.append(node.left)
-
-                if node.right:
-                    queue.append(node.right)
-            ans.append(level)
-
-        return ans
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        ans.append(level)
+    return ans
