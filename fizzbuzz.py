6d4e188f5ca2d649fbd7ebdf73ebf7662150bc9a
franklynmbongeni <ngwenyambongenifranklyn@gmail.com>
2026-08-17 21:23:07 +0200

    removed an unnecessary condition in fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,7 +6,7 @@ class Solution:
         for i in range(1, n + 1):
             if i % 3 == 0 and i % 5 == 0:
                 result.append("FizzBuzz")
-            elif i % 3 == 0 and not i % 5 == 0:
+            elif i % 3 == 0:
                 result.append("Fizz")
             elif i % 5 == 0:
                 result.append("Buzz")
